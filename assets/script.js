b7e781235013f108689d4da52d5bc29c5d08a488
The Real Mi <miaolmedillo@gmail.com>
2023-07-13 16:35:29 -0400

    added leaderboard and leaderboard button

--- assets/script.js
+++ assets/script.js
@@ -24,12 +24,14 @@ var choicesEl = document.querySelectorAll(".choices")
 var scoreEl = document.getElementById("score")
 var leaderboardEl = document.getElementById("leaderboard")
 var messageEl = document.getElementById("message")
+var leaderboardBtnEl = document.getElementById("leaderboard-btn")
 var startBtn= document.getElementById("start-btn")
 var saveBtnEl = document.getElementById("save-btn")
 var backBtnEl = document.getElementById("back-btn")
 var clearBtnEl = document.getElementById("clear-btn")
 var userInitialsEl = document.getElementById("user-initials")
 
+//Set array to store user scores
 var userScores = []
 var questionIndex = 0
 //List questions as objects in questionsArray
@@ -52,7 +54,7 @@ var questionsArray=[
     {
         title: "What is the correct way to write a JavaScript array?",
         choices: ["var dogs = ['doberman', 'shiba inu', 'golden retriever']","var dogs = 1= ('doberman'), 2= ('shiba inu'), 3= ('golden retriever')","var dogs = (1:'doberman', 2:'shiba inu', 3:'golden retriever')","var dogs = 'doberman', 'shiba inu', 'golden retriever'"],
-        answer: "C2"
+        answer: "var dogs = ['doberman', 'shiba inu', 'golden retriever']"
     },
     {
         title: "Put question5 here",
@@ -91,24 +93,30 @@ function displayQuestions(){
 //Function to change screen to display next question and display high score screen
 function nextQuestion(event) {
     var currentEl= event.target;
+    //next question happens after button click
     if (currentEl.matches("button")) {
+        //Check if answer matches user click, display correct
         if (currentEl.textContent === questionsArray[questionIndex].answer) {
             messageEl.textContent = "Correct!";
         }
+        //Check if user is wrong, display wrong and deduct 10s from timer
         else{
             messageEl.textContent = "Wrong!";
             timeLeft= timeLeft-10;
         }
         questionIndex++;
     }
+    //Set delay to display validation for users (correct, wrong)
     if (currentEl.matches("button")){
         if (questionIndex<questionsArray.length) {
             setTimeout(displayQuestions, 500);
         }
+        //if no more questions in array, show save score screen
         else{
             questionEl.classList.add("hide");
             saveScoreEl.removeAttribute("class");
             clearInterval(setIntervalId);
+            //store time left as score
             scoreEl.textContent = timerEl.textContent;
         }
 
@@ -130,22 +138,56 @@ function saveScore(){
         user: initials,
         score: timerEl.textContent
     }
+    //Append new object (user input) to userScores
     userScores.push(userObject);
+    //Add highscore to local storage
     localStorage.setItem("highScore", JSON.stringify(userScores));
+    ranking = document.createElement('li');
+    ranking.innerHTML = initials +" - "+ timerEl.textContent;
+    leaderboardEl.appendChild(ranking);
+    //Reset Timer so quiz can be taken again 
+     timeLeft = questionsArray.length * 15;
+     timerEl.textContent = 0
+    //Hide save screen
     saveScoreEl.classList.add("hide");
+    //Display high scores
     highscoresEl.removeAttribute("class");
     
 }
 
+//Function to go back to main screen
 function backToMain() {
-    
+    //Hide high scores
+    highscoresEl.classList.add("hide");
+    //Display start screen
+    startScreenEl.removeAttribute("class");
+    //Reset Question index so quiz can be taken again
+    questionIndex = 0;
 }
 
+//Clears local storage
 function clearScores() {
     localStorage.clear();
+}
+
+//Displays Leaderboard
+function viewLeaderboard() {
+    if (highscoresEl.classList.contains("hide")){
+        highscoresEl.removeAttribute("class")
+    }
+    if (!(startScreenEl.classList.contains("hide"))){
+        startScreenEl.classList.add("hide");
+    }
+    if (!(questionEl.classList.contains("hide"))){
+        questionEl.classList.add("hide");
+    }
+    if (!(saveScoreEl.classList.contains("hide"))){
+        saveScoreEl.classList.add("hide");
+    }
 }
 startBtn.addEventListener("click", startQuiz)
 questionEl.addEventListener("click", nextQuestion)
 saveBtnEl.addEventListener("click", saveScore)
 backBtnEl.addEventListener("click", backToMain)
 clearBtnEl.addEventListener("click", clearScores)
+leaderboardBtnEl.addEventListener("click", viewLeaderboard)
